dd1a207644f258988ccadd2f999a067160f533b0
MatheusTrinca <matheusfelipetrinca@gmail.com>
2024-02-11 20:32:56 -0300

    feat: :sparkles: adciona modulo de produtos

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from 'typeorm';
+import ProductRepository from '../repositories/ProductRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.', 404);
+    }
+
+    await productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,5 @@
+import { createConnection } from 'typeorm';
+
+createConnection();
+
+// 9:30
