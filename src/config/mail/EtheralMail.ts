d4095e06d1fccc3af7cde9cef5fb5839d09e4d94
MatheusTrinca <matheusfelipetrinca@gmail.com>
2024-03-04 21:22:23 -0300

    feat: :sparkles: Cria servico de email teste com Ethereal Mail

---
+++ src/config/mail/EtheralMail.ts
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer';
+
+interface ISendMail {
+  to: string;
+  body: string;
+}
+
+export default class EtherealMail {
+  static async sendMail({ to, body }: ISendMail): Promise<void> {
+    // Generate SMTP service account from ethereal.email
+    const account = await nodemailer.createTestAccount();
+
+    // Create a SMTP transporter object
+    const transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+
+    // Message object
+    const message = {
+      from: 'equipe@apivendas.com',
+      to,
+      subject: 'Recuperação de Senha',
+      text: body,
+    };
+
+    const info = await transporter.sendMail(message);
+
+    console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  }
+}
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import AppError from '@shared/errors/AppError';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtheralMail';
 
 interface IRequest {
   email: string;
@@ -20,7 +21,10 @@ class SendForgotPasswordEmailService {
 
     const token = await userTokensRepository.generate(user.id);
 
-    console.log(token);
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida ${token?.token}`,
+    });
   }
 }
 
