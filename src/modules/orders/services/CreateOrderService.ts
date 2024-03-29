13057c3ffc5b33c82fed1223d8b196ced892aa48
MatheusTrinca <matheusfelipetrinca@gmail.com>
2024-03-29 02:40:09 -0300

    feat: :sparkles: criando servico de criar pedido

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,51 @@
+import { getCustomRepository } from 'typeorm';
+import Order from '../typeorm/entities/Order';
+import OrdersRepository from '../repositories/OrdersRepository';
+import CustomersRepository from '@modules/customer/typeorm/repositories/CustomersRepository';
+import ProductRepository from '@modules/products/typeorm/repositories/ProductRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IProduct {
+  id: string;
+  price: number;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+class CreateOrderService {
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+    const customerRepository = getCustomRepository(CustomersRepository);
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const customer = await customerRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Could not find any customer with the given id', 404);
+    }
+
+    const existsProducts = await productsRepository.findAllByIds(products);
+
+    if (!existsProducts.length) {
+      throw new AppError('Could not find any product with the given ids', 404);
+    }
+
+    const existsProductsIds = existsProducts.map(product => product.id);
+
+    const checkInexistentProducts = products.filter(
+      product => !existsProductsIds.includes(product.id),
+    );
+
+    if (checkInexistentProducts.length) {
+      throw new AppError(
+        `Could not find product ${checkInexistentProducts[0].id}.`,
+      );
+    }
+  }
+}
+
+export default CreateOrderService;
